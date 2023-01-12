7c631cb5fecbdde053acd354c5e1831b5402d1c9
TETEURYAN <matheusryan61@gmail.com>
2023-01-12 15:46:25 -0300

    sorting

--- Data Structures and Algorithms/Sorting Algorithms/Recursive Bubble Sort.c
+++ Data Structures and Algorithms/Sorting Algorithms/Recursive Bubble Sort.c
@@ -1,3 +1,5 @@
+
+//first
 void swap (int *x, int *y)
 {
 	int aux = *x;
@@ -18,9 +20,26 @@ void bubble_sort (int i, int j, int array[], int n)
 				
 			bubble_sort(i, j + 1, array, n);
 		}
-		else
-		{
-			bubble_sort(i, i, array, n - 1);
-		}
+		else bubble_sort(i, i, array, n - 1);
 	}
+}
+
+//second
+
+int bubble(int arr[], int n, int i)
+{
+    if(i == n-1) return 0;
+    if(arr[i] > arr[i+1])
+    {
+        int aux = arr[i];
+        arr[i] = arr[i+1];
+        arr[i+1] = aux;
+    }
+    bubble(arr, n, i+1);
+
+int ordenar(int arr[], int n)
+{ 
+    if(n == 1) return 0;
+    bubble(arr, n, 0);
+    ordenar(arr, n-1);
 }
