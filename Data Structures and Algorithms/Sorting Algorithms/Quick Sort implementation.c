4bc7a5247d85700d523ed5b1566f8af4bf029fa5
Matheus Ryan <91018438+TETEURYAN@users.noreply.github.com>
2023-04-10 23:58:41 -0300

    Sorting
    Quick Sort implementation in C.

--- Data Structures and Algorithms/Sorting Algorithms/Quick Sort implementation.c
+++ Data Structures and Algorithms/Sorting Algorithms/Quick Sort implementation.c
@@ -10,7 +10,7 @@ void swap (int *x, int *y)
 	*y = aux;
 }
 
-int pivot(int * arr, int i, int k){
+int pivot( int i, int k){
     return (i+k)/2;
 }
 
@@ -18,7 +18,7 @@ void QuickSort(int * arr,int init,int tam){
     if (init>=tam) {
         return;
     }
-    int med = arr[pivot(arr, init, tam)];
+    int med = arr[pivot(init, tam)];
 
     int pointer = init;
     for (int i=init; i < tam; i++) {
@@ -39,12 +39,14 @@ void QuickSort(int * arr,int init,int tam){
 
 int main(){
 
+    srand(time(NULL));
+
     int tam = 50;
     int * array = calloc(tam,  sizeof(int));
 
-    for(int i = 0; i < tam-1; i++)
-         array[i] = tam-i;
-
+    for(int i = 0; i < tam; i++)
+         array[i] = rand() % tam;
+    
     QuickSort(array, 0, tam-1);
 
     for(int i = 1; i < tam; i++)
