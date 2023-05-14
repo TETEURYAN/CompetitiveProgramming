0aaf2b4dc2144ef14cc86283e214247148b5afbf
Matheus Ryan <mrysn@ic.ufal.br>
2023-05-14 12:43:39 -0300

    Queue
    Using queue method to pratice in Data Structure Class.

--- Data Structures and Algorithms/Linked List, stack, queue and struct/Queue using in a hospital context.c
+++ Data Structures and Algorithms/Linked List, stack, queue and struct/Queue using in a hospital context.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <math.h>
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
@@ -11,9 +12,6 @@
     #include <unistd.h>
 #endif
 
-
-#define MAX_SIZE 100
-
 typedef struct {
     char name[50];
     int priority; 
@@ -23,9 +21,11 @@ typedef struct {
 } Person;
 
 typedef struct {
-    Person wait[MAX_SIZE];
+    Person wait[100];
     int front, rear;
 } Queue;
+
+int tam;
 
 Person getPerson(){
     system("clear || cls");
@@ -40,9 +40,9 @@ Person getPerson(){
 
     system("clear || cls");
     
-    if (peek.age < 5 or peek.age > 80) peek.priority = 0;
+    if (peek.age < 5 or peek.age > 80) peek.priority = 2;
     else if (peek.age >= 5 and peek.age < 16 or peek.age > 60 and peek.age < 80) peek.priority = 1;
-    else peek.priority = 2;
+    else peek.priority = 0;
 
     peek.time = 0;
     peek.apend = true;
@@ -50,17 +50,21 @@ Person getPerson(){
     return peek;
 }
 
+int newPriority(int value){
+    return (not value) ? 2 : (value == 1) ? 1 : 0;
+}
+
 Person peek( Queue list){
-    return list.wait[0];
+    return list.wait[list.front];
 }
 
 bool isEmpty(Queue list){
-    return (list.front >= list.rear);
+    return (list.front > list.rear);
 }
 
 void enqueue(Queue *queue, Person patient) {
-    if (queue->rear == MAX_SIZE - 1) {
-        printf("Queue is full!\n");
+    if (queue->rear == tam-1) {
+        printf("A fila esta cheia!\n");
         return;
     }
 
@@ -87,6 +91,13 @@ void enqueue(Queue *queue, Person patient) {
     }
 }
 
+void dequeueIndex(Queue * list, int index){
+    if(index < (*list).rear){
+        (*list).wait[index] = (*list).wait[index+1];
+        dequeueIndex(list, ++index);
+    }
+    else (*list).rear--;
+}
 
 Person dequeue(Queue *queue) {
     if (queue->front == -1) {
@@ -97,15 +108,29 @@ Person dequeue(Queue *queue) {
     }
     Person patient = queue->wait[queue->front];
     patient.apend = (queue->front > queue->rear) ? false : true;
-    queue->front++;
+    queue->front += (queue->front > queue->rear) ? 0 : 1;
 
     return patient;
 }
 
-void display(Queue list, int i){
+void displayPeek(Person * peek, Queue * list, int cont, bool first){
+
+    if((*peek).apend){
+        printf("Neste momento %s esta sendo atendido | Tempo decorrido  de %d minutos.\n", (*peek).name, (*peek).time );
+    }
+    else{
+        if(not first){
+            printf("Nao ha ninguém sendo atendido agora!\n");
+            (*peek) = dequeue(list);
+        }
+    }
+    printf("Atendimentos realizado hoje: %d\n", cont);
+}
+
+void display(Queue list, int j, int i){
     if(i > list.rear) return;
-    printf("[%d] %s | Age %d | Priority %d\n", i+1, list.wait[i].name, list.wait[i].age, list.wait[i].priority);
-    display(list, ++i);
+    printf("[%d] %s | Age %d | Priority %d\n", j, list.wait[i].name, list.wait[i].age, newPriority(list.wait[i].priority));
+    display(list,++j, ++i);
 }
 
 int main() {
@@ -114,52 +139,75 @@ int main() {
     Fila.rear = -1;
 
     srand(time(NULL));
-    
+    int cont = 0;
     Person apointment;
+
+    system("clear || cls");
+    printf("Digite quantas pessoas serao antedidas no dia de hoje: ");
+    scanf("%d", &tam);
     
     bool first = true;
 
     while(1){
-        system("clear || cls");
-        printf("Opcoes:\n\n\t(1) - Inserir pessoa na fila\n\t(2) - Remover pessoa da fila\n\t(3) - Ver quem esta sendo atendido\n\t(4) - Ver fila atual\n\t(5) - Adicionar 15 minutos\n\t(0) - Sair\n\n");
-        printf("Escolha um número: ");    
-
         int op;
+        system("clear || cls");
+        printf("Opcoes:\n\n\t(1) - Inserir pessoa na fila\n\t(2) - Girar tempo\n\t(3) - Remover paciente da fila\n\t(4) - Ver fila atual\n\t(0) - Sair\n\n");
+        displayPeek(&apointment, &Fila,cont, first);
+        printf("\nEscolha um número: ");    
+
         scanf("%d", &op);
         getchar();
         switch(op){
+            case 0:
+                exit(0);
+                break;
+            
             case 1:
-                enqueue(&Fila, getPerson());
-                if(first) apointment = dequeue(&Fila);
-                first = false;
+                if(cont < tam){
+                    enqueue(&Fila, getPerson());
+                    if(first){
+                        apointment = dequeue(&Fila);
+                        cont++;
+                    }
+                    first = false;
+                }
+                else{
+                    printf("O numero maximo de antendimentos por dia foi atingido!\n");
+                    exit(0);
+                }
+
             break;
 
             case 2:
-            break;
-
-            case 3:  
-                if(apointment.apend){
-                    printf("Neste momento %s esta sendo atendido | Tempo decorrido  de %d minutos.\n", apointment.name, apointment.time );
+                if(apointment.time < 30){
+                    apointment.time += ((rand() % 15));
                 }
                 else{
-                    printf("Nao ha ninguém sendo atendido agora!\n");
                     apointment = dequeue(&Fila);
+                    cont++;
                 }
+            break;
+
+            case 3:
+            if(not isEmpty(Fila)){
+                int index;
+                printf("Digite o valor que será removido: ");
+                scanf("%d", &index);
+                dequeueIndex(&Fila, index);
+            }  
+            else{
+                printf("A fila esta vazia!\n");
                 getchar();
+            }
             break;
 
             case 4:
-            if(not isEmpty(Fila))
-                display(Fila, Fila.front);
+            if(not isEmpty(Fila) or cont == 0)
+                display(Fila, 0, Fila.front);
             else printf("A fila esta vazia!\n");    
             getchar();
             break;
         }
-        if(apointment.time < 30){
-            apointment.time += ((rand() % 15));
-        }
-        else apointment =  dequeue(&Fila);
-
     }
     return 0;
 }
