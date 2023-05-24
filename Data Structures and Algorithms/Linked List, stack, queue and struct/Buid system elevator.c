3f4a3b96bbfb197c5e1963d6746a00e2bb20c89e
Matheus Ryan <mrysn@ic.ufal.br>
2023-05-23 23:02:16 -0300

    Struct and 2D Array
    Using Struct and 2D Array to make a activiti of Data Structure Class

--- Data Structures and Algorithms/Linked List, stack, queue and struct/Buid system elevator.c
+++ Data Structures and Algorithms/Linked List, stack, queue and struct/Buid system elevator.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 #include <string.h>
+#include <limits.h>
 #include <iso646.h>
 #include <time.h>
 
 struct build{
     char floor[50];
 }build;
+
+struct coordinates{
+    int x,y;
+}coordinates;
 
 char getSymbol(int number){
     return (64 + number);
@@ -15,6 +21,28 @@ char getSymbol(int number){
 int getPos(int i, int j){
     return (j == 0) ? i :
            (j == 1) ? (i+19) : (i + 38);
+}
+
+double distTwoPoint(int x1, int y1, int x2, int y2){
+    return sqrt(((x2-x1)*(x2-x1))+((y2-y1)*(y2-y1)));
+}
+
+struct coordinates getDist(int x, int y, int ** state){
+    double small = INT_MAX;
+    struct coordinates example;
+
+    for(int i = 0; i < 300; i++){
+        for(int j = 0; j < 3; j++){
+            if(state[i][j] not_eq 0){
+                if(distTwoPoint(x,y,i,j) < small){
+                    small = distTwoPoint(x,y,i,j);
+                    example.x = i;
+                    example.y = j;
+                }
+            }
+        }
+    }
+    return example;
 }
 
 struct build * insertBuildind(struct build * architecture, int ** state){
@@ -64,14 +92,6 @@ int ** initState(int ** state){
             }
         }
     } 
-
-    for(int i = 0; i < 300; i++)
-    {
-       for(int j = 0; j < 3; j++)
-            printf("%d ", state[i][j]);
-        printf("\n");
-    }
-
     return state;
 }
 
@@ -85,6 +105,36 @@ void initFloors(struct build * architecture){
     fclose(floor);
 }
 
+void menu(int ** state, build architecture){
+
+    int op;
+
+    while(1){
+        printf("\n\tSISTEMA DE ELEVADOR\n\n");
+        printf("Opcoes:\n");
+        printf("\t(1) - Subir a partir do terreo\n\t(2) - Descer de um AP\n\t(3) - Sair\n\nEscolha:");
+
+        scanf("%d", &op);
+
+        switch(op){
+            case 1:
+
+            break;
+
+            case 2:
+
+            break;
+
+            case 3;
+                exit(0);
+            break;
+
+        }
+
+    }
+
+}
+
 int main(){
     struct build * architecture = NULL;
     int ** state = NULL;
@@ -94,4 +144,8 @@ int main(){
     architecture = insertBuildind(architecture, state);
 
     initFloors(architecture);
+
+    menu(state, architecture);
+
+    return 0;
 }
