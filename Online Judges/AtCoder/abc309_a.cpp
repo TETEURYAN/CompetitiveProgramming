beab52e9c3d01dcae01dfb4ef950ac768aead37b
Matheus Ryan <mrysn@ic.ufal.br>
2023-07-09 00:03:25 -0300

    A - Nine

---
+++ Online Judges/AtCoder/abc309_a.cpp
@@ -0,0 +1,22 @@
+        #include<bits/stdc++.h>
+         
+        using namespace std;
+         
+        int main(){
+            ios_base::sync_with_stdio(false);
+         
+            cin.tie(NULL);
+            bool ans = false;
+         
+            int n, m;
+            cin >> n >> m;
+        
+            if(n<m){
+                if((n%3)!=0){
+                    if(m-1 == n){
+                        ans = true;
+                    }
+                }
+            }
+            printf("%s\n", ans ? "Yes" : "No");
+        }
