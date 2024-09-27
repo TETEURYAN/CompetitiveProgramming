494621bc455d75fb73760a1571872340e8b75224
Matheus Ryan <mrysn@ic.ufal.br>
2024-09-26 23:53:43 -0300

    C - The Legend of Freya the Frog

---
+++ Online Judges/CodeForces/2009C.cpp
@@ -0,0 +1,17 @@
+    #include<bits/stdc++.h>
+    using namespace std;
+    typedef long long ll;
+     
+    int main() {
+    	int t; cin >> t;
+    	while (t--) {
+    		ll x, y, k;
+    		cin >> x >> y >> k;
+    		ll mx = max(x, y);
+    		ll ans = mx / k + (mx % k != 0);
+    		ans *= 2;
+    		if ((x + k - 1) / k > (y + k - 1) / k) ans--;
+    		cout << ans << "\n";
+    	}
+    	return 0;
+    }
